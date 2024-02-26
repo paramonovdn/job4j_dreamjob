9bab69be5e95728fad4795279b555e0724d775ec
paramonovdn@mail.ru <paramonovdn@mail.ru>
2024-02-26 19:31:24 +0300

    4. Многопоточность в базе данных [#504860]

---
+++ src/main/java/ru/job4j/dreamjob/controller/UserController.java
@@ -0,0 +1,41 @@
+package ru.job4j.dreamjob.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.job4j.dreamjob.dto.FileDto;
+import ru.job4j.dreamjob.model.Candidate;
+import ru.job4j.dreamjob.model.User;
+import ru.job4j.dreamjob.service.UserService;
+
+import javax.annotation.concurrent.ThreadSafe;
+
+@Controller
+@RequestMapping("/users")
+@ThreadSafe
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public String getCreationPage(Model model) {
+        return "users/register";
+    }
+
+    @PostMapping("/register")
+    public String register(Model model, @ModelAttribute User user) {
+        if (userService.findUserByEmail(user.getEmail()).isPresent()) {
+            model.addAttribute("message", "Пользователь с такой почтой уже существует");
+            return "errors/404";
+        }
+        userService.save(user);
+        return "redirect:/vacancies";
+    }
+
+}
